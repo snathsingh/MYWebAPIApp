06f82083156e11b85f52184b2e86751b29911217
Shakti <xyz@gmail.com>
2020-04-11 14:53:48 +0530

    Caching using IHttpActionResult enabling Caching for each response type

--- MyWebApiApplication/Controllers/ValuesController.cs
+++ MyWebApiApplication/Controllers/ValuesController.cs
@@ -14,32 +14,51 @@ namespace MyWebApiApplication.Controllers
     public class ValuesController : ApiController
     {
         // GET api/values
-        [AcceptVerbs("HEAD","GET")]
-        [Route("shakti/{id:Divisibleby10}", Name ="JackAss",Order =1)]
-        [ClientSideCaching(duration:20,typeOfCache:CacheType.NoCache)]
-        public IEnumerable<string> GetA(int id)
+        //[AcceptVerbs("HEAD","GET")]
+        //[Route("shakti/{id:Divisibleby10}", Name ="JackAss",Order =1)]
+        //[ClientSideCaching(duration:20,typeOfCache:CacheType.NoCache)]
+        public IEnumerable<string> GetA(int id,int b)
         {
             Thread.Sleep(2000);
             return new string[] { "value1", "value2",Request.GetApiKey(),Request.RequestUri.ToString() };
         }
 
         // GET api/values/5
-        [AcceptVerbs("VIEW","GET")]
-        //[Route("{id:Divisibleby10}")]
-        [Route("shakti1/{id:int}", Name = "JackAss1", Order = 2)]
+        //[AcceptVerbs("VIEW","GET")]
+        ////[Route("{id:Divisibleby10}")]
+        [Route("api/values/{id:int}", Name = "JackAss1", Order = 2)]
         public HttpResponseMessage Get(int id)
         {
             //return "value";            
-            string uri = Url.Link("JackAss", new { id = id });
-            var response = Request.CreateResponse(HttpStatusCode.MovedPermanently);
-            response.Headers.Location = new Uri(uri);
+            //string uri = Url.Link("JackAss", new { id = id });
+            //var response = Request.CreateResponse(HttpStatusCode.MovedPermanently);
+            //response.Headers.Location = new Uri(uri);
+            var response = Request.CreateResponse(HttpStatusCode.OK, new List<Names>() { new Names() { Name="Shakti" },new Names { Name = "Amar" },new Names { Name = "Madhuri" } });
             return response;
+        }
+        [Route("api/values/{id:int}/{b:alpha}")]
+        public IHttpActionResult Get(int id,string b)
+        {
+            //return "value";            
+            //string uri = Url.Link("JackAss", new { id = id });
+            //var response = Request.CreateResponse(HttpStatusCode.MovedPermanently);
+            //response.Headers.Location = new Uri(uri);
+            var response = Ok("Done")
+                .Cache(5,CacheType.Private);
+            return response;
+        }
+        public class Names
+        {
+            public string Name { get; set; }
         }
 
         // POST api/values
-        public void Post([FromBody]string value)
+        public HttpResponseMessage Post([FromBody]string value)
         {
-
+            var response = Request.CreateResponse(HttpStatusCode.Created,"Shakti Created");
+            var uri = Url.Link("JackAss1", new { id = 9 });
+            response.Headers.Location = new Uri(uri);
+            return response;
         }
 
         // PUT api/values/5
---
+++ MyWebApiApplication/CustomActionFilters/CacheActionResult.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+
+namespace MyWebApiApplication.CustomActionFilters
+{    
+    public class CacheActionResult : IHttpActionResult 
+    {
+        private readonly IHttpActionResult Inner;
+        private static int Duration;
+        private readonly CacheType tCache;
+        public CacheActionResult(IHttpActionResult inner,int duration,CacheType cacheType=CacheType.NoCache)
+        {
+            Inner = inner;
+            Duration = duration;
+            tCache = cacheType;
+        }       
+        public async Task<HttpResponseMessage> ExecuteAsync(CancellationToken cancellationToken)
+        {            
+            var response=await Inner.ExecuteAsync(cancellationToken);
+            if (tCache == CacheType.NoCache)
+            {
+                Duration = 0;
+                response.Headers.CacheControl = new CacheControlHeaderValue()
+                {
+                    NoStore = true
+                };
+                response.Headers.Date = DateTime.Now;
+                response.Content.Headers.Expires = response.Headers.Date;
+
+                response.Headers.Pragma.TryParseAdd("No-Cache-Boy");
+            }
+            else
+            {
+                response.Headers.CacheControl = new CacheControlHeaderValue
+                {
+                    NoCache = false,
+                    NoStore = false,
+                    Private = (CacheType.Private == tCache),
+                    Public = (CacheType.Public == tCache),
+                    MaxAge = TimeSpan.FromSeconds(Duration)
+                };
+                response.Headers.Date = DateTime.Now;
+                response.Content.Headers.Expires = response.Headers.Date+TimeSpan.FromSeconds(Duration+30);
+            }
+            return response;
+        }
+    }
+    public static class CacheExtension
+    {
+        public static IHttpActionResult Cache(this IHttpActionResult httpActionResult,int seconds,CacheType cacheType=CacheType.NoCache)
+        {
+            return new CacheActionResult(httpActionResult, seconds,cacheType);
+        }
+    }
+}
